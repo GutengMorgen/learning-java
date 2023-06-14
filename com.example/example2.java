36d2ad4364607b5372cc3149645e5df80420eaec
Kevin M <crisstian2345@gmail.com>
2023-06-14 18:19:36 -0500

    add: example 2

---
+++ com.example/example2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class example2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un precio: ");
+        int precio = scanner.nextInt();
+
+        double iva = precio * 0.21;
+        double suma = precio + iva;
+
+        System.out.println("El resultado del precio con IVA es: " + suma);
+
+        scanner.close();
+    }
+}
