8dee1647d591019c981d9f75d000fe96d372e194
Kevin M <crisstian2345@gmail.com>
2023-06-14 20:57:28 -0500

    add: task 3

---
+++ com/example/task3.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class task3 {
+
+    public static void main(String[] args) {
+
+        String[] nombres = new String[] { "Raul ", "Lia ", "Alejandro" };
+
+        for (int i = 0; i < nombres.length; i++)
+            System.out.print(nombres[i]);
+
+        System.out.println("\nEnd");
+    }
+
+}
