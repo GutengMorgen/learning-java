725041de755a01df7684393c8601ef54d3676b24
Kevin M <crisstian2345@gmail.com>
2023-06-23 21:16:54 -0500

    add: java.nio.file.File class

--- com/testing/packages.java
+++ com/testing/packages.java
@@ -1,9 +1,13 @@
 package com.testing;
 
+// import java.nio.file;
+import java.io.File;
 import static java.lang.System.*;
 
 public class packages {
     public static void main(String[] args) {
         out.println("hello world");
+        File file = new File("../example.example2.java");
+        out.print(file);
     }
 }
