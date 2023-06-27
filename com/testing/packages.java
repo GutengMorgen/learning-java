693ce778cb7a5a4e4e6a8f70089898f1bcea95cd
Kevin M <crisstian2345@gmail.com>
2023-06-26 22:59:19 -0500

    add: introduction folder

--- com/testing/packages.java
+++ com/testing/packages.java
@@ -1,26 +1,35 @@
 package com.testing;
 
 // import java.nio.file.Files;
-import java.io.File;
+// import java.io.File;
 // import java.io.InputStream;
+// import java.io.PrintStream;
+// import java.io.PrintWriter;
 
-import static java.lang.System.*;
+// import static java.lang.System.*;
 
 public class packages {
     public static void main(String[] args) {
         // out.println("hello world");
 
-        File parentDirectory = new File("D:\\TelegramDownloads2");
+        /*
+         * File parentDirectory = new File("D:\\TelegramDownloads2");
+         * 
+         * if (parentDirectory.isDirectory()) {
+         * out.println(parentDirectory + "\nit is a directory");
+         * 
+         * for (File subFiles : parentDirectory.listFiles()) {
+         * out.println(subFiles);
+         * }
+         * } else {
+         * out.println(parentDirectory + "\nit is not  a directory");
+         * }
+         */
 
-        if (parentDirectory.isDirectory()) {
-            out.println(parentDirectory + "\nit is a directory");
-
-            for (File subFiles : parentDirectory.listFiles()) {
-                out.println(subFiles);
-            }
-        } else {
-            out.println(parentDirectory + "\nit is not  a directory");
-        }
         // out.print(file.getPath());
+        System.getProperty("line.separator");
+
+        // Object foo = new Object();
+
     }
 }
